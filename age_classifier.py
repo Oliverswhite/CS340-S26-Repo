5bf5ab61053dbbe487501ccdfd7ecb5d513fad34
Kevin Angstadt <kangstadt@stlawu.edu>
2026-02-15 16:04:06 -0500

    Add Age Classifier feature

---
+++ age_classifier.py
@@ -0,0 +1,21 @@
+def classify_age(age):
+    """
+    Classifies a person based on their age.
+
+    Categories:
+    - Child: 0-12
+    - Teen: 13-19
+    - Adult: 20+
+
+    Args:
+        age (int): The age in years.
+
+    Returns:
+        str: The classification ('Child', 'Teen', or 'Adult').
+    """
+    if age < 13:
+        return "Child"
+    elif age < 19:
+        return "Teen"
+    else:
+        return "Adult"
---
+++ test_age_classifier.py
@@ -0,0 +1,15 @@
+import unittest
+from age_classifier import classify_age
+
+class TestAgeClassifier(unittest.TestCase):
+    def test_child(self):
+        self.assertEqual(classify_age(10), "Child")
+    
+    def test_mid_teen(self):
+        self.assertEqual(classify_age(15), "Teen")
+
+    def test_adult(self):
+        self.assertEqual(classify_age(25), "Adult")
+
+if __name__ == '__main__':
+    unittest.main()
