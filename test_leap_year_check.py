cc58a247bb8dec87cae3aa8dad9061b3a39edeee
Kevin Angstadt <kangstadt@stlawu.edu>
2026-02-15 16:04:05 -0500

    Add Leap Year Check feature

---
+++ test_leap_year_check.py
@@ -0,0 +1,12 @@
+import unittest
+from leap_year_check import is_leap_year
+
+class TestLeapYear(unittest.TestCase):
+    def test_simple_leap_year(self):
+        self.assertTrue(is_leap_year(2024))
+
+    def test_non_leap_year(self):
+        self.assertFalse(is_leap_year(2023))
+
+if __name__ == '__main__':
+    unittest.main()
